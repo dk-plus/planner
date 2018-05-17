82fe8b95ca4a098fb4cbd2cc05df23a3781060de
dk <dkplus@qq.com>
2018-05-18 01:00:50 +0800

    feat: 增加格式化时间

--- src/module/lib/formatTime.js
+++ src/module/lib/formatTime.js
@@ -1,6 +1,29 @@
 const formatTime = (() => {
   const _e = {};
 
+  _e.dayToSec = (day) => {
+    return day*24*60*60*1000;
+  }
+
+  _e.hourToSec = (hour) => {
+    return hour*60*60*1000;
+  }
+
+  _e.minToSec = (min) => {
+    return min*60*1000;
+  }
+
+  _e.secToDays = (sec) => {
+    let str = "";
+    let day = 24 * 60 * 60 * 1000;
+    let hour = 60 * 60 * 1000;
+    let min = 60 * 1000;
+    str = parseInt(sec/day)+'天'+parseInt(sec%day/hour)+'小时'+parseInt(sec%day%hour)+'分钟';
+    return str;
+  }
+
   _e.init = () => {}
   return _e;
 })();
+
+module.exports = formatTime;
